f2a2d42bb476fe9b328b13b88cb6df351ef51ec5
Sam Lachance <slachance@gmail.com>
2016-02-13 16:27:08 -0500

    Comments

--- app.rb
+++ app.rb
@@ -3,36 +3,32 @@ require 'feedjira'
 
 class Media
 
+  # This allows you to call all on any Class to get a list of its objects
   def self.all
     ObjectSpace.each_object(self).to_a
   end
 
+  # In order to control mplayer in slave mode it needs a file to read changes from
+  # I went ahead and just threw it in /tmp/ because it seemed like the right thing to do
+  # This checks to see if that file exists and then creates it if it doesn't
   def fifo
     unless File.exist?('/tmp/mplayer-control')
       `mkfifo /tmp/mplayer-control`
     end
   end
 
+  # This method creates the fifo file (if not already present) then, 
+  # starts mplayer in slave mode and tells it to watch the fifo file.
   def play(source)
     fifo
     `mplayer -slave -input file=/tmp/mplayer-control #{source}`
   end
 
+  # This method allows you to pass in commands for mplayer. Commands can be found with the google
   def self.mplayer(command)
     `echo "#{command}" > /tmp/mplayer-control`
   end
 end
-
-
-
-
-
-
-
-
-
-
-
 
 class Podcast < Media
   attr_accessor :name, :url, :episodes
@@ -42,39 +38,32 @@ class Podcast < Media
     @url = url
   end
 
+  # Fetches podcasts from a source url
   def fetch(source)
     Feedjira::Feed.fetch_and_parse source
   end
 
+  # Searches for an object of Podcast class by name
   def self.search_name(query)
-    a = self.all.to_a
-    a.select! { |n| n.name == query }
-    a[0]
+    a = self.all.to_a # the to_a is needed to perform the search
+    a.select! { |n| n.name == query } # Searches each object in the arracy for the object with your name
+    a[0] # Returns the object in the 0 index
   end
 
+  # Essentially utilizes the search_name method to prevent duplicate podcasts from being created
   def self.spawn(name, url)
-    if Podcast.search_name(name).nil?
-      Podcast.new(name, url)
+    if Podcast.search_name(name).nil? 
+      Podcast.new(name, url) # If a podcast with that name already exists, it will return nil
     end
   end
 
+  # Call this method on the object to store a current list of episodes in the @episodes variable
   def populate
     self.episodes = fetch(@url)
   end
-
 end
 
-
-
-
-
-
-
-
-
-
-
-
+# This is a mess
 class Radio < Media
   attr_accessor :name, :url
 
@@ -89,6 +78,7 @@ get '/' do
   erb :index
 end
 
+# The controls post to the '/' directory and then redirect back to where you came from
 post '/' do
   command = params[:command].to_s
   Media.mplayer(command)
@@ -102,12 +92,16 @@ post '/stream' do
   redirect back
 end
 
+# Below are examples of different podcasts. I wanted them to be on seperate pages because the list of episodes
+# can get long. Doing it this way also allows you to fine tune how many episodes appear in the list
+
+
 get '/sgu' do
-  Podcast.spawn("SGU", "http://www.theskepticsguide.org/feed/sgu/")
-  podcast = Podcast.search_name("SGU")
-  podcast.populate
-  @episodes = podcast.episodes.entries[0..9]
-  erb :sgu
+  Podcast.spawn("SGU", "http://www.theskepticsguide.org/feed/sgu/") #Spawns the Podcast object
+  podcast = Podcast.search_name("SGU") # Finds that object
+  podcast.populate # Populates the @episodes variable
+  @episodes = podcast.episodes.entries[0..9] # Pulls the first 10 episodes and then stores it for the view
+  erb :sgu # Renders the view
 end
 
 get '/rd' do
